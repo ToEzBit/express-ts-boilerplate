62ca66ee1440f88bdac65f29c299f12b2fe94c1e
ToEzBit <jirayut52328172@hotmail.com>
2023-07-01 19:11:54 +0700

    :sparkles: http error

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import type { Request, Response } from "express";
 
+import errorMiddleware from "./middlewares/error-middleware.js";
+
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.json({ message: "success" });
 });
 
+app.use(errorMiddleware);
+
 app.listen(8888, null, () => {
+  // eslint-disable-next-line no-console
   console.log("server is running on port 8888");
 });
---
+++ src/middlewares/error-middleware.ts
@@ -0,0 +1,15 @@
+import type { Request, Response, NextFunction } from "express";
+
+import HttpError from "../utils/http-error.js";
+
+function errorMiddleware(
+  err: HttpError,
+  req: Request,
+  res: Response,
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  next: NextFunction,
+): void {
+  res.status(err.httpStatusCode || 500).json({ message: err.errorMessage });
+}
+
+export default errorMiddleware;
